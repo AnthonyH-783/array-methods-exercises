73b3f71f7f4b7277c7379fb5ef65f488d320d2db
Anthony <anthonnhani@gmail.com>
2025-10-07 11:58:51 -0600

    Implemented filterRangeInPlace

--- arrayExercises.js
+++ arrayExercises.js
@@ -36,12 +36,36 @@ let clamp = (val_low, val_high) => {
     return function(element){
         return !(element < val_low) && !(element > val_high);
     }
-    
+}
+
+let clamp2 = function (element, val_low, val_high){
+
+        return !(element < val_low) && !(element > val_high)
+
 }
 
 
-let arr = [5, 3, 8, 1];
+//let arr = [5, 3, 8, 1];
 
-let filtered = filterRange(arr, 1, 4);
-console.log(filtered);
+//let filtered = filterRange(arr, 1, 4);
+//console.log(filtered);
+//console.log(arr);
+
+/**
+ * Write a function filterRangeInPlace(arr, a, b) that gets an array arr
+ * and removes from it all values except those that are between a and b.
+ *  The test is: a ≤ arr[i] ≤ b.
+ *  The function should only modify the array. It should not return anything.
+ */
+
+let filterRangeInPlace = (arr, a, b) => {
+
+    arr.forEach((element, index) => {
+        if(!clamp2(element, a, b)){
+            arr.splice(index, 1);
+        }  
+    });
+}
+let arr = [5, 3, 8, 1];
+let in_place_filtered = filterRangeInPlace(arr, 1, 4);
 console.log(arr);
